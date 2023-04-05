Density placement: Force scaling on voxels that are not full. Fix #27. (#28)
Density placement: Force scaling on voxels that are not full. Fix #27.

* Minor fixes in docstrings.
* Fix warnings from divide by zero. Add test to exit density placement.
* update to pandas 2.0
* Fix warnings from nan values.

---------

Co-authored-by: Mike Gevaert <michael.gevaert@epfl.ch>

atlas_densities/densities/excitatory_inhibitory_splitting.py
@@ -35,7 +35,7 @@ def scale_excitatory_densities(output, brain_regions, mapping, layer_ids, densit
         L.info("Performing layer: %s", layer)
         idx = np.nonzero(np.isin(brain_regions.raw, layer_ids[layer]))
 
-        for mtype, scale in df.iteritems():
+        for mtype, scale in df.items():
             if scale == 0:
                 continue
 