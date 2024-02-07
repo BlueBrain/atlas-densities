0646bcf125b1f746132f0b0397ebe136daf1d166
Mike Gevaert <michael.gevaert@epfl.ch>
2024-02-07 10:01:55 +0100

    remove pkg_resources, use importlib

--- atlas_densities/version.py
+++ atlas_densities/version.py
@@ -1,4 +1,4 @@
 """version"""
-from pkg_resources import get_distribution  # type: ignore
+import importlib.metadata
 
-VERSION = get_distribution("atlas_densities").version
+VERSION = importlib.metadata.version("atlas_densities")
